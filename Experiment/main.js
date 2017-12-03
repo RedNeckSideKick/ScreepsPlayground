720a9fa01cb99bd4105fee7168240a7548c043bc
RedNeckSideKick <rednecksidekick@icloud.com>
2017-12-02 21:54:46 -0700

    Reorganize
    move files into root folder

--- Experiment/main.js
+++
@@ -1,9 +0,0 @@
-var memManage = require('ops.memManage');
-var typeManagement = require('ops.typeManagement');
-var spawnCreeps = require('ops.spawnCreeps');
-
-module.exports.loop = function ()
-{
-    memManage.run();
-    typeManagement.run();
-}
